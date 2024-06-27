182ea31fa8b5af95342a95f0d422287444e6e20e
ayonpaul1992 <ayon.paul@hih7.com>
2024-06-27 12:55:57 +0530

    My fifth practice project on javascript Commit

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,20 @@
-const imgBox = document.querySelector(".img_box");
-const whiteBoxes = document.getElementsByClassName("white_box");
-imgBox.addEventListener('dragstart',(e)=>{
-    console.log("Dragstart hasbeen triggered");
-    e.target.className += " hold";
-    setTimeout(()=>{
-        e.target.className = "hide";
-    }, 0);
-});
-imgBox.addEventListener('dragend',(e)=>{
-    console.log("Dragend hasbeen triggered");
-    e.target.className = "img_box";
-});
-for(var whiteBox of whiteBoxes){
-    whiteBox.addEventListener("dragover",(e)=>{
-        e.preventDefault();
-        console.log("Dragover hasbeen triggered");
-    });   
-    whiteBox.addEventListener("dragenter",(e)=>{
-        console.log("Dragenter hasbeen triggered");
-        e.target.className += " dashed";
-    });   
-    whiteBox.addEventListener("dragleave",(e)=>{
-        console.log("Dragleave hasbeen triggered");
-        e.target.className = "white_box";
-    });   
-    whiteBox.addEventListener("drop",(e)=>{
-        console.log("Drop hasbeen triggered");
-        e.target.append(imgBox);
-    });   
+const dropArea = document.getElementById("drop-area");
+const inputFile = document.getElementById("input-file");
+const imgView = document.getElementById("img-view");
+inputFile.addEventListener("change",uploadImage);
+function uploadImage(){
+    let imglink = URL.createObjectURL(inputFile.files[0]);
+    imgView.style.backgroundImage = `url(${imglink})`;
+    imgView.textContent = "";
+    imgView.style.border = 0;
 }
+
+dropArea.addEventListener("dragover",function(e){
+    e.preventDefault();
+});
+
+dropArea.addEventListener("drop",function(e){
+    e.preventDefault();
+    inputFile.files = e.dataTransfer.files;
+    uploadImage();
+});
